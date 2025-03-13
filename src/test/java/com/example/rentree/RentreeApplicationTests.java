fa9929df72cbea3f19a320cf1f512ae438c8040c
Sun Dayeon <yeoniv7@hansung.ac.kr>
2025-03-13 16:43:58 +0900

    test

---
+++ src/test/java/com/example/rentree/RentreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
