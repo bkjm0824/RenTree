9af72dd10fb69af8fe67dfe6bbc3f2dd64fd89a3
oowhyun <qkddngus8@gmail.com>
2025-03-23 23:38:10 +0900

    로그인 화면 제작

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,1 +1,95 @@
-// 로그인 화면
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 50, horizontal: 40),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Image.asset(
+                  'assets/loginLogo.png',
+                ),
+              ),
+              SizedBox(height: 40),
+              Center(
+                child: Text(
+                  '로그인',
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff464646),
+                  ),
+                ),
+              ),
+              SizedBox(height: 80),
+
+              // 학번 입력 필드
+              Text(
+                '학번',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 5),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Color(0xff8A8282)),
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(height: 25),
+
+              // 비밀번호 입력 필드
+              Text(
+                '비밀번호',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 5),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Color(0xff8A8282)),
+                  ),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 40),
+
+              SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // 로그인 로직 추가
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xff97C663),
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text(
+                    '로그인',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
