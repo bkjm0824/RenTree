2b5d2abe812c309f77a33eca01fde3d71951464b
Sun Dayeon <yeoniv7@hansung.ac.kr>
2025-05-15 14:47:42 +0900

    대여 포인트

--- src/main/java/com/example/rentree/dto/StudentDTO.java
+++ src/main/java/com/example/rentree/dto/StudentDTO.java
@@ -11,6 +11,7 @@ public class StudentDTO {
     private String nickname; // 닉네임
     private Integer profileImage; // 프로필 이미지 난수
     private int rentalCount; // 대여 횟수
+    private int rentalPoint; // 대여 포인트
 
     static public StudentDTO fromEntity(Student student) {
         StudentDTO studentDTO = new StudentDTO(); // StudentDTO 객체 생성
@@ -20,6 +21,7 @@ public class StudentDTO {
         studentDTO.setNickname(student.getNickname()); // Student 객체의 nickname 값을 DTO에 설정
         studentDTO.setProfileImage(student.getProfileImage()); // Student 객체의 profileImage 값을 DTO에 설정
         studentDTO.setRentalCount(student.getRentalCount()); // Student 객체의 rentalCount 값을 DTO에 설정
+        studentDTO.setRentalPoint(student.getRentalPoint()); // Student 객체의 rentalPoint 값을 DTO에 설정
         return studentDTO; // StudentDTO 객체 반환
     }
 
@@ -32,6 +34,7 @@ public class StudentDTO {
                 .nickname(this.nickname)
                 .profileImage(this.profileImage)
                 .rentalCount(this.rentalCount)
+                .rentalPoint(this.rentalPoint)
                 .build();
     }
 }
